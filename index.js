039a058f733b2e5a925c7ddf3cdedb51f02c654b
gpdnjs8 <jade3735@naver.com>
2025-11-20 20:28:55 +0900

    week08 실습

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import express from "express";
 import morgan from "morgan";           
 import cookieParser from "cookie-parser"; 
+import swaggerAutogen from "swagger-autogen";
+import swaggerUiExpress from "swagger-ui-express";
 
 import { handleUserSignUp } from "./src/controllers/user.controller.js";
 import { handleAddStore, handleListStoreReviews } from "./src/controllers/store.controller.js";
@@ -34,6 +36,37 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(
+  "/docs",
+  swaggerUiExpress.serve,
+  swaggerUiExpress.setup({}, {
+    swaggerOptions: {
+      url: "/openapi.json",
+    },
+  })
+);
+
+app.get("/openapi.json", async (req, res, next) => {
+  // #swagger.ignore = true
+  const options = {
+    openapi: "3.0.0",
+    disableLogs: true,
+    writeOutputFile: false,
+  };
+  const outputFile = "/dev/null"; // 파일 출력은 사용하지 않습니다.
+  const routes = ["./index.js"];
+  const doc = {
+    info: {
+      title: "UMC 9th",
+      description: "UMC 9th Node.js 테스트 프로젝트입니다.",
+    },
+    host: "localhost:3000",
+  };
+
+  const result = await swaggerAutogen(options)(outputFile, routes, doc);
+  res.json(result ? result.data : null);
+});
+
 app.use(cors()); // cors 방식 허용
 app.use(morgan("dev")); 
 app.use(cookieParser());   
@@ -41,9 +74,9 @@ app.use(express.static("public")); // 정적 파일 접근
 app.use(express.json()); // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
 app.use(express.urlencoded({ extended: false })); // 단순 객체 문자열 형태로 본문 데이터 해석
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
 app.post("/api/v1/users/signup", handleUserSignUp);
 app.post("/api/v1/stores", handleAddStore);
